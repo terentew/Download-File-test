4a2ec08ce85b72d07d013835294040db214cef3e
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-07-09 14:54:16 +0300

    файлы

---
+++ src/test/java/tests/DownloadFiletxt.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import java.io.FileNotFoundException;
+import com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.Selenide.open;
+
+public class DownloadFiletxt {
+
+    @Test
+    void selenideDownloadFiletxtx() {
+        open("http://girhub");
+    }
+}
